847abcbbecdbb783f1212faef369faf235122472
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-04-12 20:45:21 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ function About() {
                
             </motion.div> */}
             <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}}  className='w-full'>
-                    <div className='flex justify-center lg:justify-start'>
+                    <div className='flex justify-center lg:justify-center'>
                         <p className='my-2 max-x-xl py-6'>{ABOUT_TEXT}</p>
                     </div>
                 </motion.div>
